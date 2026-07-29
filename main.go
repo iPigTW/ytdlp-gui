d92aca735bcdefa64a3b029e1b41755def7b9f5e
iPig <ipig@ipig.me>
2026-07-29 17:32:47 +0800

    added resolution selection and minor optimize

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -42,26 +43,86 @@ func main() {
 	url := widget.NewEntry()
 	url.SetPlaceHolder("Enter YouTube URL here")
 
-	format := widget.NewSelectEntry(
-		[]string{"mp3", "mp4"},
+	var resolutions []string
+
+	resolutionSelect := widget.NewSelect(
+		resolutions,
+		func(s string) {},
 	)
-	format.SetPlaceHolder("Select format")
+	resolutionSelect.Hide()
+
+	format := widget.NewSelect(
+		[]string{"mp3", "mp4"},
+		func(s string) {},
+	)
+	format.Hide()
+
+	url.OnChanged = func(s string) {
+		if strings.Contains(s, "youtube") || strings.Contains(s, "youtu.be") {
+			format.Show()
+		}
+	}
 
 	downloadStatus := widget.NewLabel("Status: None")
 
 	downloadBtn := widget.NewButtonWithIcon("Download", theme.DownloadIcon(), func() {
 		downloadStatus.SetText("Status: Downloading...")
+		if resolutionSelect.Selected != "" {
+			go func() {
+				if err := downloadVideoWithResolution(ytdlpPath.Text, outputPath.Text, format.Selected, ffmpegPath.Text, url.Text, resolutionSelect.Selected); err != nil {
+					dialog.ShowError(err, w)
+				} else {
+					fyne.Do(func() {
+						downloadStatus.SetText("Status: Download Completed")
+					})
+					dialog.ShowInformation("Completed", "File saved to "+outputPath.Text, w)
+				}
+			}()
+			return
+		}
 		go func() {
-			if err := downloadVideo(ytdlpPath.Text, outputPath.Text, format.Text, ffmpegPath.Text, url.Text); err != nil {
+			if err := downloadVideo(ytdlpPath.Text, outputPath.Text, format.Selected, ffmpegPath.Text, url.Text); err != nil {
 				dialog.ShowError(err, w)
 			} else {
-				downloadStatus.SetText("Status: Download Completed")
+				fyne.Do(func() {
+					downloadStatus.SetText("Status: Download Completed")
+				})
 				dialog.ShowInformation("Completed", "File saved to "+outputPath.Text, w)
 			}
 		}()
 	})
+	downloadBtn.Disable()
 
-	downloadUI := container.NewVBox(outputPath, outputSelect, url, format, downloadBtn, downloadStatus)
+
+
+	// only make the download button available when selected mp3 or selected mp4 and resolution
+	format.OnChanged = func(s string) {
+		if s == "mp3" {
+			resolutionSelect.Hide()
+			downloadBtn.Enable()
+			return
+		}
+		downloadBtn.Disable()
+		resolutionSelect.Show()
+
+		resolutions = nil
+		go func() {
+			resolutions, err := getResolution(ytdlpPath.Text, url.Text)
+			if err != nil {
+				dialog.ShowError(err, w)
+			}
+			fyne.Do(func() {
+				resolutionSelect.SetOptions(resolutions)
+			})
+		}()
+	}
+
+	resolutionSelect.OnChanged = func(s string) {
+		downloadBtn.Enable()
+	}
+
+
+	downloadUI := container.NewVBox(outputPath, outputSelect, url, format, resolutionSelect, downloadBtn, downloadStatus)
 
 	ytdlpPathSelect := widget.NewButtonWithIcon("Select yt-dlp executable", theme.FolderIcon(), func() {
 		fd := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
@@ -100,4 +161,40 @@ func downloadVideo(ytdlpPath string, outputPath string, format string, ffmpegPat
 		return err
 	}
 	return nil
+}
+
+func downloadVideoWithResolution(ytdlpPath string, outputPath string, format string, ffmpegPath string, url string, resolution string) error {
+	cmd := exec.Command(ytdlpPath, "-P", outputPath, "-t", format, "-S", "res:"+strings.TrimSuffix(resolution, "p"), "--ffmpeg-location", ffmpegPath, "--extractor-args", "youtube:player_js_version=actual", "--no-check-certificate", url)
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getResolution(ytdlpPath string, url string) ([]string, error) {
+	var resolution []string
+	cmd := exec.Command(ytdlpPath, "-F", url)
+	output, err := cmd.Output()
+	if err != nil {
+		return []string{}, nil
+	}
+	if strings.Contains(string(output), "1080p") {
+		resolution = append(resolution, "1080p")
+	}
+	if strings.Contains(string(output), "720p") {
+		resolution = append(resolution, "720p")
+	}
+	if strings.Contains(string(output), "480p") {
+		resolution = append(resolution, "480p")
+	}
+	if strings.Contains(string(output), "360p") {
+		resolution = append(resolution, "360p")
+	}
+	if strings.Contains(string(output), "144p") {
+		resolution = append(resolution, "144p")
+	}
+
+	return resolution, nil
 }
